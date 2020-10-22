58b7a97f8cb29cebe36d6dee337024d137a62ca6
Mike Barberry <mikebarberry@protonmail.com>
2020-10-22 05:52:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "brown", "easy going", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 13, "F", "black", "shy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "F", "black", "nervous" 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "grizzly", "outgoing",1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 11, "F", "black", "happy", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 22, "M", "brown", "mild", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 31, "M", "brown", "dominant", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 4, "M", "brown", "aggressive", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "brown", "easy going", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Rowdy", 13, "F", "black", "shy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "F", "black", "nervous" 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "grizzly", "outgoing",1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Melissa", 11, "F", "black", "happy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Grinch", 22, "M", "brown", "mild", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES ("Wendy", 31, "M", "brown", "dominant", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (NULL, 4, "M", "brown", "aggressive", 1);
