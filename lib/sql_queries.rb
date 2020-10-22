1ae2290046eb4f55e1123f5094afdc0cf016a2c0
Mike Barberry <mikebarberry@protonmail.com>
2020-10-22 06:12:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT bear.name WHERE name = NULL"
+  "SELECT bear.name FROM bears WHERE name = NULL"
 end
