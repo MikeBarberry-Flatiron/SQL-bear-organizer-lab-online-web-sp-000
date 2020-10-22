e6f3bcaf76e466b327e9b48197cb7edefd457500
Mike Barberry <mikebarberry@protonmail.com>
2020-10-22 05:40:49 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "brown", "easy going", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 13, "black", "shy", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "black", "nervous" 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 8, "grizzly", "outgoing",1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Melissa", 11, "black", "happy", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Grinch", 22, "brown", "mild", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Wendy", 31, "brown", "dominant", 0);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES (NIL, 4, "brown", "aggressive", 1);
