07efb9aa6f6fe19f70ef0f65aaa5a876c92644b0
Mike Barberry <mikebarberry@protonmail.com>
2020-10-22 05:43:21 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "brown", "easy going", 1);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 13, "black", "shy", 1);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "black", "nervous" 1);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 8, "grizzly", "outgoing",1);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Melissa", 11, "black", "happy", 1);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Grinch", 22, "brown", "mild", 1);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Wendy", 31, "brown", "dominant", 0);
-INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES (NIL, 4, "brown", "aggressive", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "brown", "easy going", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 13, "M, ""black", "shy", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 7, "F", "black", "nervous" 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "grizzly", "outgoing",1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Melissa", 11, "F", "black", "happy", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Grinch", 22, "F", "brown", "mild", 1);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ("Wendy", 31, "M", "brown", "dominant", 0);
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES (NIL, 4, "F", "brown", "aggressive", 1);
